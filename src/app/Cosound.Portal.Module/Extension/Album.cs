99d4e765f941a464e1e825019b72f5ee920c39ea
Jesper Fyhr Knudsen <Jesper@fyhr.dk>
2014-03-20 15:13:02 +0100

    Setup Module and sample extension

---
+++ src/app/Cosound.Portal.Module/Extension/Album.cs
@@ -0,0 +1,12 @@
+﻿namespace Cosound.Portal.Module.Extension
+{
+    using Chaos.Portal.Core;
+    using Chaos.Portal.Core.Extension;
+
+    public class Album : AExtension
+    {
+        public Album(IPortalApplication portalApplication) : base(portalApplication)
+        {
+        }
+    }
+}
